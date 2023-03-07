8b0148ac31cb9141705d44d4a968b697433d64ae
fadhel07 <fmapriansyah3@gmail.com>
2023-03-07 16:59:34 +0700

    .

--- footer.php
+++ footer.php
@@ -14,7 +14,7 @@
                         </ul>
                     </div>
                     <div class="col-md-6">
-                        <h6>STMIK TASIKMALAYA OK</h6>
+                        <h6>STMIK TASIKMALAYA</h6>
                         <hr class="bg-white">
                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3957.408349420106!2d108.20421701482657!3d-7.307937373877347!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6f50c5effef495%3A0x573420e273c62448!2sSTMIK%20Tasikmalaya!5e0!3m2!1sid!2sid!4v1678161503684!5m2!1sid!2sid" width="100%" height="140" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                     </div>
