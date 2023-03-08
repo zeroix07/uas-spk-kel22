c9f540e7441141ddaf5a61ebe3fb252038293fd9
fadhel07 <fmapriansyah3@gmail.com>
2023-03-08 17:22:25 +0700

    update layout and database

--- db/db_spk.sql
+++ db/db_spk.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 27, 2023 at 11:18 AM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.0.19
+-- Generation Time: Mar 08, 2023 at 10:49 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -35,7 +35,7 @@ CREATE TABLE `admin` (
   `email` varchar(200) NOT NULL,
   `username` varchar(200) DEFAULT NULL,
   `password` varchar(200) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `admin`
@@ -58,18 +58,19 @@ INSERT INTO `admin` (`id`, `nama`, `alamat`, `telepon`, `email`, `username`, `pa
 CREATE TABLE `alternatif` (
   `id_calon_kr` int(11) NOT NULL,
   `nama` varchar(200) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `alternatif`
 --
 
 INSERT INTO `alternatif` (`id_calon_kr`, `nama`) VALUES
-(7, 'IR 64'),
-(11, 'Inpari 30'),
-(12, 'Ciherang'),
-(13, 'Mekongga'),
-(14, 'Cigeulis');
+(22, 'IR64'),
+(23, 'CIHERANG'),
+(24, 'INPARI 30'),
+(25, 'MEKONGGA'),
+(26, 'CISADANE'),
+(27, 'SERTANI');
 
 -- --------------------------------------------------------
 
@@ -80,27 +81,31 @@ INSERT INTO `alternatif` (`id_calon_kr`, `nama`) VALUES
 CREATE TABLE `hasil_spk` (
   `id_spk` int(11) NOT NULL,
   `id_calon_kr` int(11) DEFAULT NULL,
-  `hasil_spk` float(10,2) DEFAULT NULL,
-  `minggu` varchar(2) NOT NULL,
-  `bulan` varchar(2) NOT NULL,
-  `tahun` varchar(4) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `hasil_spk` float(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `hasil_spk`
 --
 
-INSERT INTO `hasil_spk` (`id_spk`, `id_calon_kr`, `hasil_spk`, `minggu`, `bulan`, `tahun`) VALUES
-(45, 5, 64.25, '2', '11', '2019'),
-(46, 6, 64.25, '2', '11', '2019'),
-(47, 7, 64.25, '2', '11', '2019'),
-(48, 8, 64.25, '2', '11', '2019'),
-(49, 9, 64.25, '2', '11', '2019'),
-(50, 7, 64.25, '5', '02', '2023'),
-(51, 11, 64.25, '5', '02', '2023'),
-(52, 12, 64.25, '5', '02', '2023'),
-(53, 13, 64.25, '5', '02', '2023'),
-(54, 14, 64.25, '5', '02', '2023');
+INSERT INTO `hasil_spk` (`id_spk`, `id_calon_kr`, `hasil_spk`) VALUES
+(50, 7, 80.25),
+(51, 11, 80.25),
+(52, 12, 80.25),
+(53, 13, 80.25),
+(54, 14, 80.25),
+(55, 16, 80.25),
+(56, 21, 80.25),
+(57, 17, 80.25),
+(58, 18, 80.25),
+(59, 19, 80.25),
+(60, 20, 80.25),
+(61, 22, 84.00),
+(62, 23, 87.25),
+(63, 24, 94.00),
+(64, 25, 82.25),
+(65, 26, 76.50),
+(66, 27, 80.25);
 
 -- --------------------------------------------------------
 
@@ -111,28 +116,31 @@ INSERT INTO `hasil_spk` (`id_spk`, `id_calon_kr`, `hasil_spk`, `minggu`, `bulan`
 CREATE TABLE `hasil_tpa` (
   `id_test` int(11) NOT NULL,
   `id_calon_kr` int(11) DEFAULT NULL,
-  `Berat` float(10,2) DEFAULT NULL,
-  `Hasil_Produksi` int(11) DEFAULT NULL,
-  `Ketahanan` float(10,2) DEFAULT NULL,
-  `Tinggi` float(10,2) DEFAULT NULL,
-  `Warna` float(10,2) DEFAULT NULL,
-  `Rumpun` float(10,2) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `KETAHANAN_TERHADAP_PENYAKIT_ATAU_HAMA` float(10,2) DEFAULT NULL,
+  `PRODUKTIVITAS` float(10,2) DEFAULT NULL,
+  `WAKTU_TANAM` float(10,2) DEFAULT NULL,
+  `KUALITAS_BERAS` float(10,2) DEFAULT NULL,
+  `KETAHANAN_KEKERINGAN_ATAU_CUACA_EKSTRIM` float(10,2) DEFAULT NULL,
+  `BIAYA_PRODUKSI` float(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `hasil_tpa`
 --
 
-INSERT INTO `hasil_tpa` (`id_test`, `id_calon_kr`, `Berat`, `Hasil_Produksi`, `Ketahanan`, `Tinggi`, `Warna`, `Rumpun`) VALUES
-(55, 5, NULL, NULL, NULL, NULL, NULL, NULL),
-(56, 6, NULL, NULL, NULL, NULL, NULL, NULL),
-(57, 7, 62.00, 71, 76.00, 82.00, 85.00, 91.00),
-(58, 8, NULL, NULL, NULL, NULL, NULL, NULL),
-(59, 9, NULL, NULL, NULL, NULL, NULL, NULL),
-(61, 11, 64.00, 71, 73.00, 79.00, 85.00, 90.00),
-(62, 12, 65.00, 67, 73.00, 79.00, 83.00, 89.00),
-(63, 13, 64.00, 69, 74.00, 80.00, 85.00, 90.00),
-(64, 14, 65.00, 69, 72.00, 79.00, 85.00, 89.00);
+INSERT INTO `hasil_tpa` (`id_test`, `id_calon_kr`, `KETAHANAN_TERHADAP_PENYAKIT_ATAU_HAMA`, `PRODUKTIVITAS`, `WAKTU_TANAM`, `KUALITAS_BERAS`, `KETAHANAN_KEKERINGAN_ATAU_CUACA_EKSTRIM`, `BIAYA_PRODUKSI`) VALUES
+(66, 16, NULL, NULL, NULL, NULL, NULL, NULL),
+(67, 21, NULL, NULL, NULL, NULL, NULL, NULL),
+(68, 17, NULL, NULL, NULL, NULL, NULL, NULL),
+(69, 18, NULL, NULL, NULL, NULL, NULL, NULL),
+(70, 19, NULL, NULL, NULL, NULL, NULL, NULL),
+(71, 20, NULL, NULL, NULL, NULL, NULL, NULL),
+(72, 22, 144.00, 150.00, 154.00, 160.00, 165.00, 168.00),
+(73, 23, 145.00, 150.00, 155.00, 161.00, 164.00, 169.00),
+(74, 24, 145.00, 150.00, 154.00, 160.00, 165.00, 168.00),
+(75, 25, 145.00, 149.00, 154.00, 160.00, 166.00, 170.00),
+(76, 26, 145.00, 149.00, 155.00, 160.00, 165.00, 170.00),
+(77, 27, 145.00, 149.00, 154.00, 160.00, 165.00, 170.00);
 
 -- --------------------------------------------------------
 
@@ -142,22 +150,22 @@ INSERT INTO `hasil_tpa` (`id_test`, `id_calon_kr`, `Berat`, `Hasil_Produksi`, `K
 
 CREATE TABLE `kriteria` (
   `id_kriteria` int(11) NOT NULL,
-  `kriteria` varchar(32) DEFAULT NULL,
+  `kriteria` varchar(50) DEFAULT NULL,
   `bobot` float(5,2) DEFAULT NULL,
   `type` varchar(12) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `kriteria`
 --
 
 INSERT INTO `kriteria` (`id_kriteria`, `kriteria`, `bobot`, `type`) VALUES
-(34, 'Berat', 20.00, 'Benefit'),
-(35, 'Hasil_Produksi', 20.00, 'Benefit'),
-(36, 'Ketahanan', 15.00, 'Benefit'),
-(37, 'Tinggi', 15.00, 'Benefit'),
-(38, 'Warna', 15.00, 'Benefit'),
-(39, 'Rumpun', 15.00, 'Benefit');
+(51, 'KETAHANAN_TERHADAP_PENYAKIT_ATAU_HAMA', 15.00, 'Benefit'),
+(52, 'PRODUKTIVITAS', 25.00, 'Benefit'),
+(53, 'WAKTU_TANAM', 15.00, 'Cost'),
+(54, 'KUALITAS_BERAS', 20.00, 'Benefit'),
+(55, 'KETAHANAN_KEKERINGAN_ATAU_CUACA_EKSTRIM', 10.00, 'Benefit'),
+(56, 'BIAYA_PRODUKSI', 15.00, 'Cost');
 
 -- --------------------------------------------------------
 
@@ -170,7 +178,7 @@ CREATE TABLE `sub_kriteria` (
   `id_kriteria` int(11) NOT NULL,
   `subkriteria` varchar(255) NOT NULL,
   `nilai` float(10,2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `sub_kriteria`
@@ -206,7 +214,70 @@ INSERT INTO `sub_kriteria` (`id_subkriteria`, `id_kriteria`, `subkriteria`, `nil
 (89, 39, 'Buruk', 2.00),
 (90, 39, 'Cukup', 3.00),
 (91, 39, 'Baik', 4.00),
-(92, 39, 'Sangat Baik', 5.00);
+(92, 39, 'Sangat Baik', 5.00),
+(104, 40, 'Sangat Buruk', 1.00),
+(105, 40, 'Buruk', 2.00),
+(106, 40, 'Cukup', 3.00),
+(107, 40, 'Baik', 4.00),
+(108, 40, 'Sangat Baik', 5.00),
+(109, 41, 'Sangat Buruk', 1.00),
+(110, 41, 'Buruk', 2.00),
+(111, 41, 'Cukup', 3.00),
+(112, 41, 'Baik', 4.00),
+(113, 41, 'Sangat Baik', 5.00),
+(114, 42, 'Sangat Buruk', 1.00),
+(115, 42, 'Buruk', 2.00),
+(116, 42, 'Cukup', 3.00),
+(117, 42, 'Baik', 4.00),
+(118, 42, 'Sangat Baik', 5.00),
+(119, 43, 'Sangat Buruk', 1.00),
+(120, 43, 'Buruk', 2.00),
+(121, 43, 'Cukup', 3.00),
+(122, 43, 'Baik', 4.00),
+(123, 43, 'Sangat Baik', 5.00),
+(124, 44, 'Sangat Buruk', 1.00),
+(125, 44, 'Buruk', 2.00),
+(128, 44, 'Cukup', 3.00),
+(129, 44, 'Baik', 4.00),
+(130, 44, 'Sangat Baik', 5.00),
+(131, 45, 'Sangat Buruk', 1.00),
+(132, 45, 'Buruk', 2.00),
+(133, 45, 'Cukup', 3.00),
+(135, 45, 'Baik', 4.00),
+(136, 45, 'Sangat Baik', 5.00),
+(137, 46, 'Sangat Buruk', 1.00),
+(138, 46, 'Buruk', 2.00),
+(139, 46, 'Cukup', 3.00),
+(140, 46, 'Baik', 4.00),
+(141, 46, 'Sangat Baik', 5.00),
+(144, 51, '<= 2 Penyakit', 1.00),
+(145, 51, '3 - 4 Penyakit', 3.00),
+(146, 51, '>= 5 Penyakit', 5.00),
+(147, 52, '<= 2,5 Ton/Ha', 1.00),
+(148, 52, '2,6 - 4,5 Ton/Ha', 2.00),
+(149, 52, '4,6 - 6,5 Ton/Ha', 3.00),
+(150, 52, '6,6 - 8,5 Ton/Ha', 4.00),
+(151, 52, '>= 8,6 Ton / Ha', 5.00),
+(152, 53, '> 140 Hari', 1.00),
+(153, 53, '> 130 - 140 Hari', 2.00),
+(154, 53, '> 120 - 130 Hari', 3.00),
+(155, 53, '110 - 120 Hari', 4.00),
+(156, 53, '< 110 Hari', 5.00),
+(157, 54, 'Sangat Kurang', 1.00),
+(158, 54, 'Kurang', 2.00),
+(159, 54, 'Cukup Baik', 3.00),
+(160, 54, 'Baik', 4.00),
+(161, 54, 'Sangat Baik', 5.00),
+(162, 55, 'Sangat Kurang', 1.00),
+(163, 55, 'Kurang', 2.00),
+(164, 55, 'Cukup Baik', 3.00),
+(165, 55, 'Baik', 4.00),
+(166, 55, 'Sangat Baik', 5.00),
+(167, 56, '> 15.000.000', 1.00),
+(168, 56, '> 12.500.000 - 15.000.000', 2.00),
+(169, 56, '> 10.000.000 - 12.500.000', 3.00),
+(170, 56, '7.500.000 - 10.000.000', 4.00),
+(171, 56, '< 7.500.000', 5.00);
 
 --
 -- Indexes for dumped tables
@@ -264,31 +335,31 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `alternatif`
 --
 ALTER TABLE `alternatif`
-  MODIFY `id_calon_kr` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id_calon_kr` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT for table `hasil_spk`
 --
 ALTER TABLE `hasil_spk`
-  MODIFY `id_spk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
+  MODIFY `id_spk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=67;
 
 --
 -- AUTO_INCREMENT for table `hasil_tpa`
 --
 ALTER TABLE `hasil_tpa`
-  MODIFY `id_test` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=65;
+  MODIFY `id_test` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=78;
 
 --
 -- AUTO_INCREMENT for table `kriteria`
 --
 ALTER TABLE `kriteria`
-  MODIFY `id_kriteria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+  MODIFY `id_kriteria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
 
 --
 -- AUTO_INCREMENT for table `sub_kriteria`
 --
 ALTER TABLE `sub_kriteria`
-  MODIFY `id_subkriteria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=93;
+  MODIFY `id_subkriteria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=172;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
